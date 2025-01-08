d1ae10214e5f66aa4c44691e82f2f77017f89ef2
aniruddramesh <143079942+aniruddramesh@users.noreply.github.com>
2025-01-08 22:37:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function changeBackground(imageName) {
     const body = document.body;
     body.style.backgroundImage = `url('${imageName}')`;
-    body.style.backgroundSize = 'cover';
+    body.style.backgroundSize = 'contain';
     body.style.backgroundRepeat = 'no-repeat';
     body.style.backgroundPosition = 'center';
     body.style.backgroundAttachment = 'fixed';
@@ -11,8 +11,8 @@ function changeBackground(imageName) {
 const galleryImages = {
     mysore: ["./images/mp1.jpg", "./images/mp2.jpg", "./images/mp3.jpg"],
     jaganmohan: ["./images/jm1.jpg", "./images/jm2.jpg", "./images/jm3.jpg"],
-    krs: ["./images/krs1.jpg", "./images/krs2.jpg", "./images/krs3.jpg"],
-    shivanasamudra: ["./images/ss1.jpg", "./images/ss2.jpg", "./images/ss3.jpg"]
+    krs: ["https://imgs.search.brave.com/CE05G6fZaCDT_ikfMOJmN93BxWAXadJguc68G2KoHCg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9mYXN0/bHkuNHNxaS5uZXQv/aW1nL2dlbmVyYWwv/NjAweDYwMC8xNTMy/OTA0Nl9JV1ZOd1dX/ZUo3WlJLMm1kanpH/eUJBOS1LMTkyalYw/OURQaGZFT3k4NDNz/LmpwZw", "https://imgs.search.brave.com/tlHFhWphPMWkaYd3N9jYYdGcKgDW9lnzCda7k8AZe7M/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZGVjY2FuY2hy/b25pY2xlLmNvbS9k/Yy1Db3Zlci1mbHZ1/OTlhYXNhdGd1Zjhq/amQ5YjNucWQzMy0y/MDE3MDIwNzAzNDE1/MC5NZWRpLmpwZWc", "./images/krs3.jpg"],
+    shivanasamudra: ["./images/shiva1.jpg", "./images/shiva2.jpg", "./images/ss3.jpg"]
 };
 
 function changeImage(gallery, direction) {
